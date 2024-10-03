a007a004673551712c695e2f3a3e3b8695c5e4e9
mohamed <mohamed@utopios.net>
2024-10-03 09:47:06 +0200

    correction tp 20

--- 5-boucles/exercices/correction-exercice20-step2.sh
+++ 5-boucles/exercices/correction-exercice20-step2.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
 
+dossier_source="Dossier1"
+
+mkdir -p "$dossier_source/Medias"
+mkdir -p "$dossier_source/Docs"
+mkdir -p "$dossier_source/Travail"
+
+echo "$dossier_source"
+
+for file in "$dossier_source"/*; do
+
+    if [[ -f $file  ]]; then
+
+        extension="${file##*.}"
+
+        case $extension in
+            jpg|png|mp4)
+            mv "$file" "$dossier_source/Medias/"
+            ;;
+            docx|pptx|txt|pdf)
+            mv "$file" "$dossier_source/Docs/"
+            ;;
+            log|sh|csv)
+            mv "$file" "$dossier_source/Travail/"
+            ;;
+            *)
+            echo "Extension non reconnue $file"
+            ;;
+        esac
+    fi
+done
+
+echo Fichiers triés avec succès.
