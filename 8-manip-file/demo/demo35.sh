8ccfb4d3a95194306cc6c4dd9fa64d46e1cba8e5
mohamed <mohamed@utopios.net>
2024-10-03 11:54:26 +0200

    add demo manip file script

--- 8-manip-file/demo/demo35.sh
+++ 8-manip-file/demo/demo35.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-filename=
+filename="salary.csv"
+
+while IFS=',' read -r id employee_id first_name last_name job_title salary
+do
+    echo "ID: $id | ID EMPLOYE : $employee_id | Prenom : $first_name | Nom : $last_name | Job : $job_title | Salaire : $salary "
+done < "$filename"
