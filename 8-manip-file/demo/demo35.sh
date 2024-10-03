3091e3eb2ccbba17c6b78c464de4a832fb156de6
mohamed <mohamed@utopios.net>
2024-10-03 12:02:49 +0200

    add demo script manip

--- 8-manip-file/demo/demo35.sh
+++ 8-manip-file/demo/demo35.sh
@@ -4,5 +4,7 @@ filename="salary.csv"
 
 while IFS=',' read -r id employee_id first_name last_name job_title salary
 do
-    echo "ID: $id | ID EMPLOYE : $employee_id | Prenom : $first_name | Nom : $last_name | Job : $job_title | Salaire : $salary "
+    if [[ $job_title == "VP Sales" ]]; then
+         echo "ID: $id | Prenom : $first_name | Salaire : $salary "
+    fi
 done < "$filename"
